461a256410a98f5453eff8febfc416e380ff48e0
Serge Medvid <mserge@newtonideas.com>
2024-06-24 20:48:01 +0300

    Update OpenAI class as ChatCompletion is no longer supported with package versions > 1.0

--- chat/openai.py
+++ chat/openai.py
@@ -1,19 +1,17 @@
 from interfaces.chat import IChat
-import openai
+from openai import OpenAI
 
 from interfaces.memory_factory import IMemoryFactory
 
 class OpenAIChat(IChat):
     def __init__(self, openai_api_key: str, memory_factory: IMemoryFactory):
         self.openai_api_key = openai_api_key
-        openai.api_key = self.openai_api_key
         self.memory = memory_factory.create_memory()
+        self.client = OpenAI(api_key=self.openai_api_key)
 
     def get_response(self, input_text: str) -> str:
         # Retrieve structured messages from memory
         structured_messages = self.memory.get_messages()
-        
-        print(structured_messages)
         
         # Convert structured messages to the format expected by OpenAI API
         messages_for_openai = []
@@ -25,10 +23,7 @@ class OpenAIChat(IChat):
         messages_for_openai.append({"role": "user", "content": input_text})
         
         # Call the OpenAI API with the prepared messages
-        response = openai.ChatCompletion.create(
-            model="gpt-4o",
-            messages=messages_for_openai
-        )
+        response = self.client.chat.completions.create(model="gpt-4o", messages=messages_for_openai)
         
         # Return the response content if available, otherwise return "No response"
-        return response.choices[0].message['content'] if response.choices else "No response"
+        return response.choices[0].message.content if response.choices else "No response"
